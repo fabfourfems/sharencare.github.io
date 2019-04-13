4dc3730f60d9bd898d4ad351e78e73b213e28f8b
Lior Levy <lior7212@gmail.com>
2019-04-13 16:22:20 -0700

    added table

--- src/components/FoodbankDashboard.js
+++ src/components/FoodbankDashboard.js
@@ -3,6 +3,9 @@ import ReactChartkick, { ColumnChart, PieChart } from 'react-chartkick'
 import Chart from 'chart.js'
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import FoodbankDataTable from './FoodbankDataTable';
+import Button from '@material-ui/core/Button';
+
 
 ReactChartkick.addAdapter(Chart)
 
@@ -21,25 +24,36 @@ class FoodbankDashboard extends Component {
 
     render() {
         return (
-            <div className="row">
-                <div>
-                    <h2 id="title">Supply Tracker</h2>
-                    <ColumnChart id="categorychart" data={[["Produce", 32], ["Canned Food", 46], ["Pantry Goods", 28]]} />
-                </div>
-                <div id = "lowon">
-                    <Card>
-                        <CardContent id="setOrange" className ="content">
-                            <h2>Low On</h2>
-                        </CardContent>
-                    </Card>
+            <div>
+                <div className="row">
+                    <div>
+                        <h2 id="title">Supply Tracker</h2>
+                        <ColumnChart id="categorychart" data={[["Produce", 32], ["Canned Food", 46], ["Pantry Goods", 28]]} />
+                    </div>
+                    <div id="lowon">
+                        <Card>
+                            <CardContent id="setOrange" className="content">
+                                <h2>Low On</h2>
+                            </CardContent>
+                        </Card>
 
-                    <Card id ="expiring">
-                        <CardContent  id="setYellow" className ="content">
-                            <h2>Expiring Soon</h2>
-                     
-                        </CardContent>
-                    </Card>
+                        <Card id="expiring">
+                            <CardContent id="setYellow" className="content">
+                                <h2>Expiring Soon</h2>
+
+                            </CardContent>
+                        </Card>
+                    </div>
                 </div>
+
+                <div className="table">
+                    <Button variant="contained" id="add">Add Item</Button>
+                    <FoodbankDataTable />
+                </div>
+
+                <br />
+                <br />
+
             </div>
         );
     }
