c3f0fa2760cfc4c19a5defd2235c853aa31dd476
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-04-13 12:58:42 -0700

    Added new components

---
+++ src/components/Foodbanks.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import HomeCard from './components/HomeCard';
+
+class FoodBanks extends Component {
+    componentDidMount() {
+        // read from firebase
+        this.setState({
+            foodbankInfo: {}
+        })
+    }
+
+    makeCards() {
+        let cards = [];
+        Object.keys(this.state.foodbankInfo).forEach((item) => {
+            let info = this.state.foodbankInfo[item];
+            cards.push(<HomeCard/>);
+        });
+    }
+
+    render() {
+        let cards = this.makeCards();
+        return(
+            <div>
+                {cards}
+            </div>
+        );
+    } 
+}
