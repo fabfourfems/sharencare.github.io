c81fbe6e2e6c49c0dec9a04324cf7d07d8f31ac6
Harshitha Akkaraju <harshi.akkaraju@gmail.com>
2019-04-13 12:36:55 -0700

    Added more skeleton code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
-import HomeCard from './components/HomeCard';
+import { HashRouter as Router, Switch, Redirect, Route } from 'react-router-dom';
+import Routes from './routes';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  signin() {
+    return (
+      <div>
+        Signin
+      </div>
+    );
+  }
+
+  signup() {
+    return (
+      <div>
+        sign up
+      </div>
+    );
+  }
+
+  browseView() {
+
+  }
+
+  foodbankHome() {
+
+  }
+
   render() {
     return (
-      <div className="App">
-        <p>sharencare</p>
-        <HomeCard/>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path={Routes.signin} component={() => this.signin()} />
+          <Route exact path={Routes.signup} component={() => this.signup()} />
+        </Switch>
+      </Router>
     );
   }
 }
