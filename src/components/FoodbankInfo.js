5891dc6bf31bc1e645d9d483e59db7ce33797835
saihn <saihn@uw.edu>
2019-04-13 16:19:33 -0700

    food bank info

--- src/components/FoodbankInfo.js
+++ src/components/FoodbankInfo.js
@@ -1,6 +1,19 @@
 import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+import FoodCard from './FoodCard';
 
 class FoodBankInfo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            address: "",
+            mission: ""
+        }
+    }
+
+    componentDidMount() {}
+
     componentWillReceiveProps(props) {
         /**
          * name
@@ -10,10 +23,37 @@ class FoodBankInfo extends Component {
         // this.setState({
         //     name: props.name,
         // })
+        this.setState({
+            name: props.name,
+            address: props.address,
+            mission: props.mission
+        });
     }
+
     render() {
         return(
-            <p>{this.state.name}</p>
+            <div>
+            <div>
+              <h1>{this.name}</h1>
+              <p>Mission:</p>
+              <p>{this.mission}</p>
+              <p>Location:</p>
+              <p>{this.address}</p>
+              <p className="paragraph">What we need</p>
+              <FoodCard/>
+              <p className="paragraph">What we have</p>
+              <FoodCard/>
+              <br/>
+              <br/>
+            </div>
+
+            <div>
+              <Button>Give!</Button>
+              <Button>Volunteer!</Button>
+            </div>
+            </div>
         );
     }
 }
+
+export default FoodBankInfo;
